1c199373f2ae258c80341388c30ccb8a2c65ee10
GabrielDittrich <gdittrchcj@gmail.com>
2024-10-22 10:00:15 -0300

    Alfabeto usando os Números em Bytes

---
+++ Aula18/AlfabetoComByte.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 65;
+
+    for (int i = 65; i <= 90; i++)
+    {
+        printf("%c \t", i);
+    }
+
+    printf("\n\n65 em bytes é igual a: %c", a);
+
+    return 0;
+}
